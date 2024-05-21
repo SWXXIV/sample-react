86afac4fb15742aca38875ebf73117f93e0db57f
Nick Kelly <k05nk01@gmail.com>
2024-05-21 16:16:31 -0700

    using class components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,53 @@
 import React from "react";
 
 export default function App() {
-  const getElement = (weather: string): JSX.Element => {
-    const element = <h1>The weather is {weather}</h1>;
-    return element;
+
+  interface WeatherProps {
+    weather: string;
+  }
+
+  type WeatherState = {
+    count: number;
   };
-  return getElement("sunny");
+
+  class WeatherComponent extends React.Component<WeatherProps, WeatherState> {
+    constructor(props: WeatherProps) {
+      super(props);
+      this.state = {
+        count: 0
+      };
+    }
+
+    componentDidMount() {
+      this.setState({ count: 1});      
+    }
+
+    clickHandler(): void {
+      this.setState({ count: this.state.count + 1});
+    }
+
+    render() {
+      return (
+        <h1 onClick={() => this.clickHandler()}>
+          The weather is {this.props.weather}, and the counter shows{" "}
+          {this.state.count}
+        </h1>
+      );
+    }
+  }
+  // const clickHandler = (text: string): void => {
+  //   alert(text);
+  // };
+
+  // const WeatherComponent = (props: WeatherProps): JSX.Element => {
+  //   const text = `The weather is ${props.weather}`;
+  //   return (<h1 onClick={() => clickHandler(text)}>{text}</h1>);
+  // };
+
+  return (<WeatherComponent weather="sunny" />);
+  // const getElement = (weather: string): JSX.Element => {
+  //   const element = <h1>The weather is {weather}</h1>;
+  //   return element;
+  // };
+  // return getElement("sunny");
 }
